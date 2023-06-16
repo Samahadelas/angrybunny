ccf5ad0eb156293ff8248e393d0a35b7538cc15f
Samah Abousharieha <samah.abousharieha@angstrom.uu.se>
2023-06-16 11:31:58 +0200

    use_testthat?

--- R/test.R
+++ R/test.R
@@ -12,3 +12,6 @@
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
+test_that("strsplit1() splits a string", {
+  expect_equal(strsplit1("a,b,c", split = ","), c("a", "b", "c"))
+})
